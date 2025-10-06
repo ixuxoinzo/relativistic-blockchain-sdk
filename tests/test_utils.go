2f0da7d461b011c9d1ce6b0a6d24b2b406924ecb
ixuxoinzo <rikeajipangestu2@gmail.com>
2025-10-06 17:00:15 +0700

    Create test_utils.go

---
+++ tests/test_utils.go
@@ -0,0 +1,65 @@
+package tests
+
+import (
+	"time"
+
+	"github.com/ixuxoinzo/relativistic-blockchain-sdk/pkg/types"
+)
+
+func CreateTestNode(id string, lat, lon float64) *types.Node {
+	return &types.Node{
+		ID: id,
+		Position: types.Position{
+			Latitude:  lat,
+			Longitude: lon,
+			Altitude:  0,
+		},
+		Address: id + ".example.com:8080",
+		Metadata: types.Metadata{
+			Region:   "test",
+			Provider: "test",
+			Version:  "1.0.0",
+		},
+		IsActive: true,
+		LastSeen: time.Now().UTC(),
+	}
+}
+
+func CreateTestBlock(hash, proposedBy string, lat, lon float64) *types.Block {
+	return &types.Node{
+		Hash:      hash,
+		Timestamp: time.Now().UTC(),
+		ProposedBy: proposedBy,
+		NodePosition: types.Position{
+			Latitude:  lat,
+			Longitude: lon,
+			Altitude:  0,
+		},
+		Data: []byte("test data"),
+	}
+}
+
+func CreateTestTransaction(hash string, lat, lon float64) *types.Transaction {
+	return &types.Transaction{
+		Hash:      hash,
+		Timestamp: time.Now().UTC(),
+		NodePosition: types.Position{
+			Latitude:  lat,
+			Longitude: lon,
+			Altitude:  0,
+		},
+		Data: []byte("test transaction"),
+	}
+}
+
+func GenerateTestNodes(count int, startLat, startLon float64) []*types.Node {
+	nodes := make([]*types.Node, count)
+	for i := 0; i < count; i++ {
+		nodes[i] = CreateTestNode(
+			"node-"+string(i),
+			startLat+float64(i)*0.1,
+			startLon+float64(i)*0.1,
+		)
+	}
+	return nodes
+}
