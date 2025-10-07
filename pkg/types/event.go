5d1c73b09290240ad5ed6c792e6f9f73c8ab83ce
ixuxoinzo <rikeajipangestu2@gmail.com>
2025-10-07 15:59:35 +0700

    Delete pkg/types/event.go

--- pkg/types/event.go
+++
@@ -1,9 +0,0 @@
-package types
-
-const (
-    AlertTypeDiscoveryIssue = "discovery_issue"
-    AlertTypeNetworkPartition = "network_partition"
-    AlertSeverityCritical = "critical"
-    AlertSeverityWarning = "warning"
-    AlertSeverityInfo = "info"
-)
