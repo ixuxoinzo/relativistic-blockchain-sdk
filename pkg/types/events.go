8c0dfc0ddc0aec8b6540e0926435deeb2796d944
ixuxoinzo <rikeajipangestu2@gmail.com>
2025-10-07 15:43:34 +0700

    Create events.go

---
+++ pkg/types/events.go
@@ -0,0 +1,7 @@
+const (
+    AlertTypeDiscoveryIssue = "discovery_issue"
+    AlertTypeNetworkPartition = "network_partition"
+    AlertSeverityInfo = "info"
+    AlertSeverityWarning = "warning" 
+    AlertSeverityCritical = "critical"
+)
